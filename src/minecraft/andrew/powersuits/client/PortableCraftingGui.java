3cc715a2ad84434812ae5650a490f5121808b97d
Andrew2448 <andrewbruce1997@gmail.com>
2013-03-17 03:17:23 -0400

    Fixed MCP rename

--- src/minecraft/andrew/powersuits/client/PortableCraftingGui.java
+++ src/minecraft/andrew/powersuits/client/PortableCraftingGui.java
@@ -32,7 +32,7 @@ public class PortableCraftingGui extends GuiContainer {
         // TODO Variable-ize this
         int var4 = this.mc.renderEngine.getTexture("/gui/crafting.png");
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.renderEngine.func_98187_b("/gui/crafting.png");
+        this.mc.renderEngine.bindTexture("/gui/crafting.png");
         int var5 = (this.width - this.xSize) / 2;
         int var6 = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(var5, var6, 0, 0, this.xSize, this.ySize);
