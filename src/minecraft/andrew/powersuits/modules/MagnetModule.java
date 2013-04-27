11dfbe7fe90d58bd828d00efb29066b11e94e807
Andrew2448 <andrewbruce1997@gmail.com>
2013-04-27 12:21:01 -0400

    Remove magnet module for now because it's broken. Will be rewritten hopefully soon.

--- src/minecraft/andrew/powersuits/modules/MagnetModule.java
+++ src/minecraft/andrew/powersuits/modules/MagnetModule.java
@@ -1,7 +1,6 @@
 package andrew.powersuits.modules;
 
 import andrew.powersuits.common.AddonComponent;
-import andrew.powersuits.common.AddonUtils;
 import net.machinemuse.api.IModularItem;
 import net.machinemuse.api.ModuleManager;
 import net.machinemuse.api.moduletrigger.IPlayerTickModule;
@@ -11,12 +10,9 @@ import net.machinemuse.powersuits.powermodule.PowerModuleBase;
 import net.machinemuse.utils.ElectricItemUtils;
 import net.machinemuse.utils.MuseCommonStrings;
 import net.machinemuse.utils.MuseItemUtils;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class MagnetModule extends PowerModuleBase implements IPlayerTickModule, IToggleableModule {
@@ -56,72 +52,6 @@ public class MagnetModule extends PowerModuleBase implements IPlayerTickModule, 
         if (ElectricItemUtils.getPlayerEnergy(player) > 100) {
             if ((player.worldObj.getTotalWorldTime() % 20) == 0) {
                 ElectricItemUtils.drainPlayerEnergy(player, ModuleManager.computeModularProperty(stack, MAGNET_ENERGY_CONSUMPTION));
-            }
-            World world = player.worldObj;
-            float distancexz = 6;
-            float distancey = 6;
-            double maxspeedxz = 0.5;
-            double maxspeedy = 0.5;
-            double speedxz = 0.05;
-            double speedy = 0.07;
-
-            List<EntityItem> items = world.getEntitiesWithinAABB(EntityItem.class, player.boundingBox.expand(distancexz, distancey, distancexz));
-            for (Iterator<EntityItem> iterator = items.iterator(); iterator.hasNext(); ) {
-                EntityItem item = iterator.next();
-
-                if (item.delayBeforeCanPickup > 0) {
-                    continue;
-                }
-                if (item.isDead && world.isRemote) {
-                    iterator.remove();
-                }
-
-                if (!AddonUtils.canItemFitInInventory(player, item.getEntityItem())) {
-                    continue;
-                }
-
-                double dx = player.posX - item.posX;
-                double dy = player.posY + player.getEyeHeight() - item.posY;
-                double dz = player.posZ - item.posZ;
-                double absxz = Math.sqrt(dx * dx + dz * dz);
-                double absy = Math.abs(dy);
-                if (absxz > distancexz) {
-                    continue;
-                }
-
-                if (absxz > 1) {
-                    dx /= absxz;
-                    dz /= absxz;
-                }
-
-                if (absy > 1) {
-                    dy /= absy;
-                }
-
-                double vx = item.motionX + speedxz * dx;
-                double vy = item.motionY + speedy * dy;
-                double vz = item.motionZ + speedxz * dz;
-
-                double absvxz = Math.sqrt(vx * vx + vz * vz);
-                double absvy = Math.abs(vy);
-
-                double rationspeedxz = absvxz / maxspeedxz;
-                if (rationspeedxz > 1) {
-                    vx /= rationspeedxz;
-                    vz /= rationspeedxz;
-                }
-
-                double rationspeedy = absvy / maxspeedy;
-                if (rationspeedy > 1) {
-                    vy /= rationspeedy;
-                }
-
-                if (absvxz < 0.2 && absxz < 0.2 && world.isRemote) {
-                    item.setDead();
-                }
-                if (AddonUtils.isClientSide()) {
-                    item.setVelocity(vx, vy, vz);
-                }
             }
         }
     }
