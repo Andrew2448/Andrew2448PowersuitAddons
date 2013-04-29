a50df57363c9438b3184d23649361b392a1c274e
Andrew2448 <andrewbruce1997@gmail.com>
2013-04-29 18:34:49 -0400

    Possible fix for issue #5 - Computer chip not craftable.

--- src/minecraft/andrew/powersuits/common/AddonRecipeManager.java
+++ src/minecraft/andrew/powersuits/common/AddonRecipeManager.java
@@ -58,6 +58,7 @@ public class AddonRecipeManager {
             ItemStack advMachine = ModCompatability.getIC2Item("advancedMachine");
             ItemStack gen = ModCompatability.getIC2Item("generator");
             ItemStack ovscanner = ModCompatability.getIC2Item("ovScanner");
+            ovscanner.setItemDamage(27);
             ItemStack goldCable = ModCompatability.getIC2Item("doubleInsulatedGoldCableItem");
 
             //===========================================================================================================================
@@ -74,6 +75,7 @@ public class AddonRecipeManager {
             ItemStack reinforcedGlass = ModCompatability.getIC2Item("reinforcedGlass");
             ItemStack advCircuit = ModCompatability.getIC2Item("advancedCircuit");
             ItemStack ovscanner = ModCompatability.getIC2Item("ovScanner");
+            ovscanner.setItemDamage(27);
 
             //===========================================================================================================================
             GameRegistry.addRecipe(new ShapedOreRecipe(AddonComponent.solarPanel, true, "GGG", "PCP", 'G', reinforcedGlass, 'P', "plateAlloyIridium", 'C', energyFlowCircuit));
