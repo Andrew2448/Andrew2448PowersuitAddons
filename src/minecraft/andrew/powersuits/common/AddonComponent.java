dc3f19984ffff61a06fbcd9c3ff677c9b9629d25
Andrew2448 <therealandrew2448@gmail.com>
2013-05-04 16:36:52 -0400

    Added a null check on ModularPowersuits.components to prevent a crash.

--- src/minecraft/andrew/powersuits/common/AddonComponent.java
+++ src/minecraft/andrew/powersuits/common/AddonComponent.java
@@ -1,5 +1,6 @@
 package andrew.powersuits.common;
 
+import net.machinemuse.general.MuseLogger;
 import net.machinemuse.powersuits.common.ModularPowersuits;
 import net.minecraft.item.ItemStack;
 
@@ -9,9 +10,14 @@ public class AddonComponent {
     public static ItemStack computerChip;
 
     public static void populate() {
-        solarPanel = ModularPowersuits.components.addComponent("componentSolarPanel", "Solar Panel", "A light sensitive device that will generate electricity from the sun.", "solarpanel");
-        magnet = ModularPowersuits.components.addComponent("componentMagnet", "Magnet", "A metallic device that generates a magnetic field which pulls items towards the player.", "magnetb");
-        computerChip = ModularPowersuits.components.addComponent("componentComputerChip", "Computer Chip", "An upgraded control circuit that contains a CPU which is capable of more advanced calculations.", "computerchip");
+        if (ModularPowersuits.components != null) {
+            solarPanel = ModularPowersuits.components.addComponent("componentSolarPanel", "Solar Panel", "A light sensitive device that will generate electricity from the sun.", "solarpanel");
+            magnet = ModularPowersuits.components.addComponent("componentMagnet", "Magnet", "A metallic device that generates a magnetic field which pulls items towards the player.", "magnetb");
+            computerChip = ModularPowersuits.components.addComponent("componentComputerChip", "Computer Chip", "An upgraded control circuit that contains a CPU which is capable of more advanced calculations.", "computerchip");
+        }
+        else {
+            MuseLogger.logError("MPS components were not initialized, MPSA componenets will not be activated.");
+        }
     }
 
 }
