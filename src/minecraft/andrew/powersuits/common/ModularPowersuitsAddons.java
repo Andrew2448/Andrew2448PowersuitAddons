792fefaa85533141301076006d6ac6a238315940
master801 <lazy_boi_301@hotmail.com>
2013-08-15 13:30:32 -0500

    1.6.2 Half Update
    Changed, @PostInit, @Init, and @PreInit to @EventHandler.

--- src/minecraft/andrew/powersuits/common/ModularPowersuitsAddons.java
+++ src/minecraft/andrew/powersuits/common/ModularPowersuitsAddons.java
@@ -3,10 +3,8 @@ package andrew.powersuits.common;
 import andrew.powersuits.book.ItemBook;
 import andrew.powersuits.network.AndrewPacketHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.Mod.PostInit;
-import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
@@ -31,7 +29,7 @@ public class ModularPowersuitsAddons {
     @SidedProxy(clientSide = "andrew.powersuits.client.ClientProxy", serverSide = "andrew.powersuits.common.CommonProxy")
     public static CommonProxy proxy;
 
-    @PreInit
+    @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         AddonConfig.setConfigFolderBase(event.getModConfigurationDirectory());
         AddonConfig.initItems();
@@ -39,7 +37,7 @@ public class ModularPowersuitsAddons {
         //proxy.readManuals();
     }
 
-    @Init
+    @EventHandler
     public void load(FMLInitializationEvent event) {
         //book = new ItemBook(AddonConfig.manualID);
         AddonComponent.populate();
@@ -50,7 +48,7 @@ public class ModularPowersuitsAddons {
         NetworkRegistry.instance().registerGuiHandler(this, guiHandler);
     }
 
-    @PostInit
+    @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         AddonRecipeManager.addRecipes();
         AddonConfig.getConfig().save();
